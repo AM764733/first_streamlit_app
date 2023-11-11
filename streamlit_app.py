8da426cc0eb17886722d8a919fcb7b37db37791a
Aman Varshney <48066620+AM764733@users.noreply.github.com>
2023-11-11 20:10:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ streamlit.title('My parent new healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('HArd-Boiled Free-Range Egg')
